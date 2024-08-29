fd7e2ca6651e5b02be2803f84473e4dbf67b5f75
Suvidha <142667232+SuvidhaDeswal02@users.noreply.github.com>
2024-08-29 23:23:05 +0530

    prime number or not

---
+++ recursion/prime_num.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+bool isPrime (int n){
+    for(int i=2;i<n;i++){
+        if(n%i==0) return 0;
+    }
+    return 1;
+}
+int main()
+{
+    int n;
+    cout<<"Enter the number:";
+    cin>>n;
+    cout<<isPrime(n);
+   
+
+    return 0;
+}
